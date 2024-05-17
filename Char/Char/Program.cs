88e84ed70f062e928d53ca90ae4901466d194b07
PRDG-k <lasrb6605@naver.com>
2024-05-17 23:33:45 +0900

    day4

---
+++ Char/Char/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Char
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            char a = '안';
+            char b = '녕';
+            char c = '하';
+            char d = '세';
+            char e = '요';
+
+            Console.Write($"{a}{b}{c}{d}{e}");
+        }
+    }
+}
