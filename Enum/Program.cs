cc583a7623aade71863436c450c8e367915b3ee0
PRDG-k <lasrb6605@naver.com>
2024-05-22 00:52:14 +0900

    day 6

---
+++ Enum/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Enum
+{
+    class MainApp
+    {
+        enum DialogResult { YES, NO, CANCEL, Confirm, OK }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine((int)DialogResult.YES);
+            Console.WriteLine((int)DialogResult.NO);
+            Console.WriteLine((int)DialogResult.CANCEL);
+            Console.WriteLine((int)DialogResult.Confirm);
+            Console.WriteLine((int)DialogResult.OK);
+        }
+    }
+}
